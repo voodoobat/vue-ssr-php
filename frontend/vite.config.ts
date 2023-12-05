2569b30c9f3fa1a626c1ce739071d90afa56ac03
Vova Rodin <mynameisvova@yahoo.com>
2023-12-05 12:55:53 +0300

    update build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
+    },
     build: {
         manifest: true,
         // https://rollupjs.org/configuration-options/
